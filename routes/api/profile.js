74d9f5dbaf5a89c0d769124afc006f6bf60f069b
ben <benoit.rejou@coding-academy.fr>
2020-08-29 14:55:46 +0200

    Adding Delete Profile & User

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -142,4 +142,23 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+//@route    DELETE api/profile/
+//@desc     Delete profile, user & post
+//@access   Public
+router.delete("/", auth, async (req, res) => {
+  try {
+    //@todo -remove user Posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
