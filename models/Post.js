afaccf87c9642d7713718d0178c69004306c215c
ben <benoit.rejou@coding-academy.fr>
2020-08-30 15:50:13 +0200

    Adding Add Post route

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,9 @@
-const moongose = (require = "mongoose");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
-    ref: "users",
   },
   text: {
     type: String,
@@ -20,7 +19,6 @@ const PostSchema = new Schema({
     {
       user: {
         type: Schema.Types.ObjectId,
-        ref: "users",
       },
     },
   ],
@@ -28,7 +26,6 @@ const PostSchema = new Schema({
     {
       user: {
         type: Schema.Types.ObjectId,
-        ref: "users",
       },
       text: {
         type: String,
@@ -52,4 +49,4 @@ const PostSchema = new Schema({
   },
 });
 
-module.exports = Post = mongoose.model("post", PostSchema);
+module.exports = mongoose.model("post", PostSchema);
